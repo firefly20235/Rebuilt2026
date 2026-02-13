316f9a19770b06cdc607546dddf3c84ad34df746
shlomitush <shlomit.talorot@gmail.com>
2026-02-13 04:33:24 +0200

    Shooter command

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -8,65 +8,89 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.DoubleSupplier;
+
+import static frc.robot.RobotContainer.SWERVE;
 
 public class Shooter extends SubsystemBase {
 
-    final TalonFX rightShooterMotor = new TalonFX(20);
-    final TalonFX leftShooterMotor = new TalonFX(21);
-
-    final TalonFX feederMotor = new TalonFX(22);
-
+    static final TalonFX rightShooterMotor = new TalonFX(20);
+    static final TalonFX leftShooterMotor = new TalonFX(21);
+    static final TalonFX feederMotor = new TalonFX(22);
     final Servo angleServo = new Servo(0);
-
-    final VelocityDutyCycle velocityRequest = new VelocityDutyCycle(0);
+    static final VelocityDutyCycle velocityRequest = new VelocityDutyCycle(0);
 
 
     public Shooter() {
         leftShooterMotor.setControl(
                 new Follower(rightShooterMotor.getDeviceID(), false)
         );
-
-
     }
+
+    public static FunctionalCommand ShootCommand(AngularVelocity targetSpeed) {
+        return new FunctionalCommand(
+
+                () -> {
+                    setShooterSpeed(
+                            targetSpeed.in(Units.Rotations.per(Units.Second)));
+                },
+
+                () -> {
+                    if (isAtSpeed(targetSpeed)) {
+                        feedFuel();
+                    } else {
+                        holdFuel();
+                    }
+                },
+
+                interrupted -> {
+                    stopShooter();
+                },
+
+                () -> false,
+
+                this
+        );
+    }
+
 
     public void setAngle(Angle angle) {
         angleServo.setAngle(angle.in(Units.Degrees));
     }
 
-    public void holdFuel() {
+    public static void holdFuel() {
         feederMotor.set(ShooterConstants.MOTOR_HOLD_VALUE);
     }
 
 
-    public void feedFuel() {
+    public static void feedFuel() {
         feederMotor.set(ShooterConstants.MOTOR_FEED_VALUE);
     }
 
-    public AngularVelocity getShooterSpeed() {
+    public static AngularVelocity getShooterSpeed() {
         return rightShooterMotor.getVelocity().getValue();
     }
 
-    public void setShooterSpeed(double targetRPS) {
+    public static void setShooterSpeed(double targetRPS) {
         rightShooterMotor.setControl(
                 velocityRequest.withVelocity(targetRPS)
         );
     }
 
-    public void stopShooter() {
+    public static void stopShooter() {
         rightShooterMotor.stopMotor();
         leftShooterMotor.stopMotor();
         feederMotor.stopMotor();
     }
 
-    public boolean isAtSpeed(AngularVelocity targetShooterRPS) {
+    public static boolean isAtSpeed(AngularVelocity targetShooterRPS) {
 
         AngularVelocity currentSpeed = getShooterSpeed();
 
-        double error = Math.abs(
-                currentSpeed.minus(targetShooterRPS)
-                        .in(Units.Rotations.per(Units.Second))
-        );
+        AngularVelocity error = currentSpeed.minus(targetShooterRPS);
 
         return error < ShooterConstants.SHOOTER_TOLERANCE_RPS;
     }
